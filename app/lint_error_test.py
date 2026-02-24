39f417b89febaf8c5dc19a22926d7cabf4efcc57
Ahmed Khalil Ebrahim Jasim <its.ahmad09@gmail.com>
2026-02-24 05:42:19 +0000

    fix: Remove lint errors

--- app/lint_error_test.py
+++
@@ -1,7 +0,0 @@
-# This file intentionally has lint errors for CI testing
-import    os      # Multiple spaces - E271
-import sys
-
-def bad_function(  ):  # E201, E202
-    x=1+2  # E225
-    return x
